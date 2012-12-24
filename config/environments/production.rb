52387e486276487ffd6113789794900f94efaa94
vzl <schamane@i.ua>
2012-12-24 20:37:29 +0000

    turn off auto compile on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Aphtekas::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
 #  config.assets.compile = false
   config.assets.precompile += %w( *.js *.css )
-  config.assets.compile = true
+  config.assets.compile = false
 
 
   # Generate digests for assets URLs
